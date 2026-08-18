ef48f0556d890753059214a70731b4459cf2388b
SnigChan21 <snigdachandanala@gmail.com>
2026-08-18 20:09:06 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.java
@@ -0,0 +1,20 @@
+class MyHashSet {
+    public List<Integer> list;
+    public MyHashSet() {
+        list=new ArrayList<>();
+    }
+    
+    public void add(int key) {
+        if (!list.contains(key)) {
+            list.add(key);
+        }
+    }
+    
+    public void remove(int key) {
+        list.remove(Integer.valueOf(key));
+    }
+    
+    public boolean contains(int key) {
+        return list.contains(key);
+    }
+}
